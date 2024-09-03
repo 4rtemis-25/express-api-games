d613be220c04fb4dcf37b869fff531729e563116
4rtemis-25 <4rtemis-25@users.noreply.github.com>
2024-09-02 22:30:12 -0500

    crud terminado

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,34 +1,53 @@
 import { pool } from "../db.js";
 
 export const getGames = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM games')
-    res.json(rows)
+    try {
+        const [rows] = await pool.query('SELECT * FROM games')
+        res.json(rows)
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Algo salio mal'
+        })
+    }
 }
 
 export const createGames = async (req, res) => {
     const { name, price } = req.body
-    const [rows] = await pool.query('INSERT INTO games (name, price) VALUES (?, ?)', [name, price])
+    try {
+        const [rows] = await pool.query('INSERT INTO games (name, price) VALUES (?, ?)', [name, price])
     res.send({
         id: rows.insertId,
         name,
         price
     })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Algo salio mal'
+        })
+    }
 }
 
 export const getGamesById = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM games WHERE id = ?', [req.params.id])
+    try {
+        const [rows] = await pool.query('SELECT * FROM games WHERE id = ?', [req.params.id])
 
     if(rows.length <= 0) return res.status(404).json({
         message: 'Game Not Found'
     })
     res.json(rows[0])
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Algo salio mal'
+        })
+    }
 }
 
 export const updateGames = async (req, res) => {
     const { id } = req.params
     const { name, price } = req.body
 
-    const [result] = await pool.query('UPDATE games SET name = IFNULL(?, name), price = IFNULL(?, price) WHERE id = ?', [name, price, id]) 
+    try {
+        const [result] = await pool.query('UPDATE games SET name = IFNULL(?, name), price = IFNULL(?, price) WHERE id = ?', [name, price, id]) 
     
     console.log(result);
     
@@ -40,15 +59,27 @@ export const updateGames = async (req, res) => {
     const [rows] = await pool.query('SELECT * FROM games WHERE id = ?', [id])
 
     res.json(rows[0])
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Algo salio mal'
+        })
+    }
+    
     
 } 
 
 export const deleteGames = async (req, res) => {
-    const result = await pool.query('DELETE FROM games WHERE id = ?', [req.params.id])
+    try {
+        const result = await pool.query('DELETE FROM games WHERE id = ?', [req.params.id])
 
     if(result.affectedRows <= 0) return res.status(404).json({
         message: 'Game not found'
     })
 
     res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Algo salio mal'
+        })
+    }
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/api/v1', gamesRoutes)
 app.use(indexRoutes)
+app.use('/api/v1', gamesRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Ruta no encontrada'
+    })
+})
 
 app.listen(3000)
